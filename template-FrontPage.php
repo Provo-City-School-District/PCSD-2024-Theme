19b9b31e0d53da4997ba75499476fcff81247df4
Josh Espinoza <joshespi@gmail.com>
2023-12-07 10:54:00 -0700

    eliminated dulplicated image folder in assets

--- template-FrontPage.php
+++ template-FrontPage.php
@@ -133,7 +133,7 @@ $get_to_know_fields = get_fields();
 									} elseif (has_post_thumbnail()) {
 										the_post_thumbnail();
 									} else { ?>
-										<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/building-image.jpg'; ?>" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" width="217" height="175">
+										<img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/building-image.jpg'; ?>" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" width="217" height="175">
 									<?php } ?>
 
 								</div>
