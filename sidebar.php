15f3dbf743ce67a61160306cc650fb9f7279bad0
Josh Espinoza <joshespi@gmail.com>
2023-12-18 09:06:00 -0700

    additional sidebar work for mockup

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,20 @@
 <aside id="rightSidebar" class="rightSidebar">
-
+<?php
+// gather child theme variables
+$theme_vars = my_theme_variables();
+?>
 	<h2>Follow Us</h2>
 	<ul class="sociallinks">
-		<li><a href="https://www.facebook.com/provoschooldistrict/"><img src="//globalassets.provo.edu/image/icons/facebook.svg" alt="Link to Facebook" /></a></li>
-		<li><a href="https://www.instagram.com/provocityschooldistrict/"><img src="//globalassets.provo.edu/image/icons/instagram.svg" alt="Link to Instagram" /></a></li>
-		<li><a href="https://twitter.com/ProvoSchoolDist"><img src="//globalassets.provo.edu/image/icons/twitter.svg" alt="Link to Twitter" /></a></li>
+	<li><a href=""><?php echo get_svg('socialmedia-insta'); ?></a></li>
+				<li><a href=""><?php echo get_svg('socialmedia-twitter'); ?></a></li>
+				<li><a href=""><?php echo get_svg('socialmedia-facebook'); ?></a></li>
 	</ul>
 <h2>A/B Calendar</h2>
 <?php echo do_shortcode( '[calendar id="2064"]' ); ?>	
 <h2>Important Dates</h2>
+<section class="impDates">
+<?php echo do_shortcode( '[calendar id="2066"]' ); ?>	
 
-<?php echo do_shortcode( '[calendar id="2064"]' ); ?>	
+</section>
+
 </aside>
