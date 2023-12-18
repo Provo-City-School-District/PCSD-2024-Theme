68c076d8801bcea185adaa7e7ec3e5dc49c57ca8
Josh Espinoza <joshespi@gmail.com>
2023-12-18 10:54:43 -0700

    changed sidebar to pull from variables

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,26 @@
 <aside id="rightSidebar" class="rightSidebar">
-<?php
-// gather child theme variables
-$theme_vars = my_theme_variables();
-?>
+	<?php
+	// gather child theme variables
+	$theme_vars = my_theme_variables();
+	?>
 	<h2>Follow Us</h2>
 	<ul class="sociallinks">
-	<li><a href=""><?php echo get_svg('socialmedia-insta'); ?></a></li>
-				<li><a href=""><?php echo get_svg('socialmedia-twitter'); ?></a></li>
-				<li><a href=""><?php echo get_svg('socialmedia-facebook'); ?></a></li>
+		<li><a href="<?php echo $theme_vars['insta_link'] ?>"><?php echo get_svg('socialmedia-insta'); ?></a></li>
+		<li><a href="<?php echo $theme_vars['facebook_link'] ?>"><?php echo get_svg('socialmedia-twitter'); ?></a></li>
+		<li><a href="<?php echo $theme_vars['twitter_link'] ?>"><?php echo get_svg('socialmedia-facebook'); ?></a></li>
 	</ul>
-<h2>A/B Calendar</h2>
-<?php echo do_shortcode( '[calendar id="2064"]' ); ?>	
-<h2>Important Dates</h2>
-<section class="impDates">
-<?php echo do_shortcode( '[calendar id="2066"]' ); ?>	
-
-</section>
+	<?php
+	//load sidebar calendars
+	if (isset($theme_vars['top_sidebar_cal'])) {
+		echo '<h2>A/B Calendar</h2>';
+		echo do_shortcode($theme_vars['top_sidebar_cal']);
+	}
+	if (isset($theme_vars['bot_sidebar_cal'])) {
+		echo '<section class="impDates">';
+		echo '<h2>Important Dates</h2>';
+		echo do_shortcode($theme_vars['bot_sidebar_cal']);
+		echo '</section>';
+	}
+	?>
 
 </aside>
