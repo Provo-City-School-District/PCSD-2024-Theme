ec29397b4fd2772610afe9f7674fca9690541b42
Josh Espinoza <joshespi@gmail.com>
2024-07-18 15:41:10 -0600

    display sidebar if it exists

--- sidebar.php
+++ sidebar.php
@@ -5,9 +5,11 @@
 	?>
 	<h2>Follow Us</h2>
 	<ul class="sociallinks">
-	<li><a href="<?php echo $theme_vars['insta_link'] ?>"><?php echo get_svg('socialmedia-insta'); ?></a></li>
-				<!-- <li><a href="<?php //echo $theme_vars['twitter_link'] ?>"><?php //echo get_svg('socialmedia-twitter'); ?></a></li> -->
-				<li><a href="<?php echo $theme_vars['facebook_link'] ?>"><?php echo get_svg('socialmedia-facebook'); ?></a></li>
+		<li><a href="<?php echo $theme_vars['insta_link'] ?>"><?php echo get_svg('socialmedia-insta'); ?></a></li>
+		<!-- <li><a href="<?php //echo $theme_vars['twitter_link'] 
+							?>"><?php //echo get_svg('socialmedia-twitter'); 
+																		?></a></li> -->
+		<li><a href="<?php echo $theme_vars['facebook_link'] ?>"><?php echo get_svg('socialmedia-facebook'); ?></a></li>
 	</ul>
 	<?php
 	//load sidebar calendars
@@ -20,6 +22,11 @@
 		echo '<h2>Important Dates</h2>';
 		echo do_shortcode($theme_vars['bot_sidebar_cal']);
 		echo '</section>';
+	}
+
+	//load sidebar menu if it exists
+	if (function_exists('sidebar_menu')) {
+		sidebar_menu();
 	}
 	?>
 
